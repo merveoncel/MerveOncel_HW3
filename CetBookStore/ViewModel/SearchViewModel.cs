5ae53c1a59198b66c7a24a91ea7b8229dff27557
Merve Oncel <mrvncll@gmail.com>
2020-04-30 13:03:17 +0300

    Proje dosyası ekle.

---
+++ CetBookStore/ViewModel/SearchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewModel
+{
+    public class SearchViewModel
+    {
+         [Display(Name ="Arama Metni")]
+        public string SearchText { get; set; }
+        [Display(Name ="Açıklamalarda Ara")]
+        public bool SearchInDescription { get; set; }
+        [Display(Name ="Kategori Seçimi")]
+        public int? CategoryId { get; set; }
+        [Display(Name = "En dusuk fiyat")]
+        public int? MinimumPrice { get; set; }
+        [Display(Name = "En yuksek fiyat")]
+        public int? MaxPrice { get; set; }
+        public List<Models.Book> Results { get; set; }
+    }
+}
